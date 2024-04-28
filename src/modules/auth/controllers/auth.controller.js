966083f193c7c71be2ebdaf76f0b4b65ed070ae1
MBRBota <117132555+MBRBota@users.noreply.github.com>
2024-04-28 22:24:27 +0300

    feat: add access token refresh route

--- src/modules/auth/controllers/auth.controller.js
+++ src/modules/auth/controllers/auth.controller.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import registerController from "./register.controller.js";
 import loginController from "./login.controller.js";
+import tokenController from "./token.controller.js";
 
 const router = Router();
 
 router.use(registerController)
 router.use(loginController)
+router.use(tokenController)
 
 export default router;
---
+++ src/modules/auth/controllers/token.controller.js
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import sql from "../../../database/db.js";
+import jwt from 'jsonwebtoken';
+import HttpError from "../../../models/HttpError.js";
+import { generateAccessToken } from "../services/generateToken.service.js";
+
+const router = Router();
+
+router.get('/token', async (req, res, next) => {
+  try{
+    const refreshToken = req.cookies.jwtRefreshToken;
+  
+    if (!refreshToken)
+      throw new HttpError("Missing refresh token.", 401)
+
+    const [foundUser] = await sql`SELECT username, refresh_token FROM users WHERE refresh_token=${ refreshToken }`
+    if (!foundUser)
+      throw new HttpError("Invalid token.", 403)
+
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      (err, decoded) => {
+        if (err || decoded.username !== foundUser.username)
+          throw new HttpError("Invalid token.", 403)
+        const accessToken = generateAccessToken(decoded.username)
+
+        return res.status(200).json({
+          success: true,
+          message: `Access token refreshed.`,
+          accessToken: accessToken
+        })
+      }
+    )
+  } catch (err) {
+    next(err)
+  }
+
+})
+
+export default router
