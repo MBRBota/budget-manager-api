7ff4609b23051af0299fd655c1576cf7955f2c59
MBRBota <117132555+MBRBota@users.noreply.github.com>
2024-04-28 23:44:01 +0300

    feat: implement cookie-parser middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import api from './api/api.js';
 import { errorHandler } from './middleware/errorHandler.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
 app.use(express.json());
+
+app.use(cookieParser())
 
 app.use('/api', api);
 
