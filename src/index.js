8095357611ad89ec4f9a65e3c7eef6be986f8eaf
MBRBota <117132555+MBRBota@users.noreply.github.com>
2024-05-01 20:25:32 +0300

    fix: implement CORS to allow communication with frontend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import api from './api/api.js';
+import cors from 'cors';
 import { errorHandler } from './middleware/errorHandler.middleware.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3500;
+
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 app.use(express.json());
 
