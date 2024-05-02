4654476f17636bfc389dbcddcae98cd8e2c5a8ba
MBRBota <117132555+MBRBota@users.noreply.github.com>
2024-05-03 01:37:12 +0300

    feat: add category GET and POST endpoints

--- src/modules/budget/controllers/budget.controller.js
+++ src/modules/budget/controllers/budget.controller.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
+import { verifyToken } from "../../../middleware/verifyToken.middleware.js";
+import categoryController from "./category.controller.js";
+import expenseController from "./expense.controller.js";
 
 const router = Router();
 
+router.use(verifyToken)
+router.use('/category', categoryController)
+router.use('/expense', expenseController)
 
 
 export default router;
--- src/modules/budget/controllers/category.controller.js
+++ src/modules/budget/controllers/category.controller.js
@@ -1,7 +1,42 @@
 import { Router } from "express";
+import HttpError from "../../../models/HttpError";
 
 const router = Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const username = req.username
 
+    const userCategories = await sql`SELECT category_id, category_name, category_color FROM categories LEFT JOIN users USING (user_id) WHERE username=${username} OR user_id IS NULL`
+
+    return res.status(200).json({
+      success: true,
+      message: "Categories retrieved successfully",
+      data: [...userCategories]
+    })
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const username = req.username
+    const { categoryName, categoryColor } = req.body.category
+
+    if (!categoryName || !categoryColor)
+      throw new HttpError("New category information incomplete/missing.", 400)
+
+    const [userId] = await sql`SELECT user_id FROM users WHERE username=${username}`
+    await sql`INSERT INTO categories (category_name, category_color, user_id) VALUES (${categoryName}, ${categoryColor}, ${userId})`
+
+    return res.status(201).json({
+      success: true,
+      message: "Category added successfully."
+    })
+  } catch (err) {
+    next(err)
+  }
+})
 
 export default router;
