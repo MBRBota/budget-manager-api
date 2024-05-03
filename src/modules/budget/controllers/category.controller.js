218bffd3b2b11e7d79a343b142fe36060f6d145d
MBRBota <117132555+MBRBota@users.noreply.github.com>
2024-05-03 15:29:09 +0300

    feat: add category PATCH and DELETE endpoints

--- src/modules/budget/controllers/category.controller.js
+++ src/modules/budget/controllers/category.controller.js
@@ -39,4 +39,44 @@ router.post('/', async (req, res) => {
   }
 })
 
+router.patch('/', async (req, res) => {
+  try{
+    const username = req.username
+    const { categoryId, categoryName, categoryColor } = req.body.category
+
+    if (!categoryId || !categoryName || !categoryColor)
+      throw new HttpError("Missing information for category update.", 400)
+
+    const [userId] = await sql`SELECT user_id FROM users WHERE username=${username}`
+    await sql`UPDATE categories SET category_name=${categoryName}, category_color=${categoryColor} WHERE user_id=${userId}`
+
+    return res.status(200).json({
+      success: true,
+      message: "Category updated successfully."
+    })
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/', async (req, res) => {
+  try{
+    const username = req.username
+    const { categoryId } = req.body.category
+
+    if (!categoryId)
+      throw new HttpError("Missing category ID.", 400)
+
+    const [userId] = await sql`SELECT user_id FROM users WHERE username=${username}`
+    await sql`DELETE FROM categories WHERE category_id=${categoryId} AND user_id=${userId}`
+
+    return res.status(200).json({
+      success: true,
+      message: "Category deleted successfully."
+    })
+  } catch (err) {
+    next(err)
+  }
+})
+
 export default router;
