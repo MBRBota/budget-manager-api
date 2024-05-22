1c1b620e7df83e8d73b4e9deb4abcb656408e91f
MBRBota <117132555+MBRBota@users.noreply.github.com>
2024-05-23 01:04:40 +0300

    fix: correctly parse request body data for expense patch and delete routes

--- src/modules/budget/controllers/expense.controller.js
+++ src/modules/budget/controllers/expense.controller.js
@@ -76,24 +76,38 @@ router.post('/', async (req, res, next) => {
 router.patch('/', async (req, res, next) => {
   try{
     const username = req.username
-    const { expenseId, expenseSum, expenseDate, categoryId } = req.body.category
+    const { expenseId, expenseSum, expenseDate, categoryId } = req.body.expense
 
     if (!expenseId || !expenseSum || !expenseDate || !categoryId)
       throw new HttpError("Missing information for expense update.", 400)
 
     const [{user_id}] = await sql`SELECT user_id FROM users WHERE username=${username}`
-    await sql`
-      UPDATE expenses
-      SET
-        expense_sum=${expenseSum}, expense_date=${expenseDate}, category_id=${categoryId}
-      WHERE
-        user_id=${user_id}
-        AND expense_id=${expenseId}
-    `
+    const [updatedExpense] = await sql`
+      WITH updated AS (
+        UPDATE expenses
+        SET
+          expense_sum=${expenseSum}, expense_date=${expenseDate}, category_id=${categoryId}
+        WHERE
+          user_id=${user_id}
+          AND expense_id=${expenseId}
+        RETURNING *
+      )
+      SELECT
+        expense_id AS "expenseId",
+        expense_sum AS "expenseSum",
+        expense_date AS "expenseDate",
+        category_id AS "categoryId",
+        category_name AS "categoryName",
+        category_color AS "categoryColor"
+      FROM updated
+      INNER JOIN categories
+      USING (category_id)
+      `
 
     return res.status(200).json({
       success: true,
-      message: "Expense updated successfully."
+      message: "Expense updated successfully.",
+      data: { updatedExpense }
     })
   } catch (err) {
     next(err)
@@ -103,7 +117,7 @@ router.patch('/', async (req, res, next) => {
 router.delete('/', async (req, res, next) => {
   try{
     const username = req.username
-    const { expenseId } = req.body.category
+    const expenseId = req.body.expense
 
     if (!expenseId)
       throw new HttpError("Missing expense ID.", 400)
